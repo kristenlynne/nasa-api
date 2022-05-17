a265091b23596b1c3830773de8174056fefac3b3
Kristen Smith <kristennlynne@gmail.com>
2022-05-16 22:56:29 -0700

    updated project

--- js/main.js
+++ js/main.js
@@ -12,17 +12,41 @@ function getFetch(){
         console.log(data);
         if(data.media_type === 'image'){
           document.querySelector('img').src = data.hdurl
+          document.querySelector('img').classList.remove('hidden');
+          document.querySelector('iframe').classList.add('hidden');
         } else if (data.media_type === 'video'){
           document.querySelector('iframe').src = data.url
+          document.querySelector('iframe').classList.remove('hidden');
+          document.querySelector('img').classList.add('hidden');
         }
         document.querySelector('#content-container').classList.remove('hidden');
         document.querySelector('p').innerText = data.explanation
         document.querySelector('h3').innerText = data.title
-        
+        document.querySelector('a').href = data.url
+        // document.querySelector('span').innerText = data.copyright // some of them don't have copyright so it displays undefined, we don't want that so I won't add this until I figure out to fixed that.
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
 
+// disables user from being able to select future dates in datepicker
+
+let todayDate = new Date();
+let month = todayDate.getMonth() + 1; // current month
+let year = todayDate.getFullYear(); // current year
+let day = todayDate.getDate(); // current date
+if(month < 10){
+  month = '0' + month
+}
+if(day < 10){
+  day = '0' + day
+}
+let maxDate = year + '-' + month + '-' + day;
+document.querySelector('#currentDate').setAttribute('max', maxDate);
+let date = document.querySelector('#currentDate').value = maxDate;
+
+$(window).load(function() {
+  $("body").removeClass("preload");
+});
  
